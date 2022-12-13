ffe91ec22dd0a959418b75c772ba68659fe9e0dc
@julin.freitas <julio.freitas@solides.com.br>
2022-12-13 14:33:58 -0300

    fix(fix): fix url api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
-const isProduction = process.env?.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 const API = axios.create({
-  baseURL: isProduction
-    ? 'https://listing-frontendmentor.vercel.app/api'
-    : 'http://localhost:3000/api'
+  baseURL: isProduction ? '/api' : 'http://localhost:3000/api'
 });
 
 export default API;
